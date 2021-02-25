252afdbff4e278175a7bff20319a29bd71f84701
Amit Kumar <asrece.86@gmail.com>
2021-02-25 09:04:09 +0530

    problem 42

---
+++ problems/problem42/explantaion.md
@@ -0,0 +1,4 @@
+# Explanation
+We first take a mirror image of the matrix vertically, then we take a mirror imgae along diagonal.
+
+
---
+++ problems/problem42/solution.py
@@ -0,0 +1,9 @@
+def rotate(matrix):
+    matrix.reverse()
+    for i in range(len(matrix)):
+        for j in range(i):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+matrix  = [[1,2,3],[4,5,6],[7,8,9]]
+rotate(matrix)
+print(matrix)
