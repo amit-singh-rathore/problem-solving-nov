dbeb8c7fb18106dd7debb843b5f6bf8a8afeef0c
Amit Kumar <asrece.86@gmail.com>
2021-03-27 19:43:18 +0530

    problem106

---
+++ problems/106-MergeIntervals/explantaion.md
@@ -0,0 +1,6 @@
+# Explanation
+
+First we arrange the intervals in the ascending order.
+Then we compare set the min of left and max of right for overlapping intervals.
+
+
---
+++ problems/106-MergeIntervals/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        res = []
+        intervals.sort()
+        res.append(intervals[0])
+        for interval in intervals[1:]:
+            if interval[0] <= res[-1][1]:
+                res[-1][1] = max(res[-1][1], interval[1])
+            else:
+                res.append(interval)
+        return res
+        
