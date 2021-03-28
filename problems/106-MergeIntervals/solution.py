ab9be9e0c54f9b9dc2a678a84c71e06a925036c5
Amit Kumar <asrece.86@gmail.com>
2021-03-28 11:44:04 +0530

    problem106 with alternative

--- problems/106-MergeIntervals/solution.py
+++ problems/106-MergeIntervals/solution.py
@@ -1,12 +1,48 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        res = []
-        intervals.sort()
-        res.append(intervals[0])
-        for interval in intervals[1:]:
-            if interval[0] <= res[-1][1]:
-                res[-1][1] = max(res[-1][1], interval[1])
-            else:
-                res.append(interval)
-        return res
         
+        intervals.sort(key = lambda x: x[0])
+        
+        res = [intervals[0]]
+        
+        for idx in range(1,len(intervals)):
+            cur = intervals[idx]
+            last = res[-1]
+            
+            if last[1] < cur[0]:
+                res.append(cur)
+            else:
+                last[1] = max(last[1], cur[1])
+            
+        return res
+ 
+# class Solution:
+# def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+    
+#     mapping  = defaultdict(int)
+#     ans = []
+    
+#     for interval in intervals:
+#         mapping[interval[0]] += 1
+#         mapping[interval[1]] -= 1
+    
+        
+#     startpt = -1 
+#     endpt = -1
+#     freq = 0
+    
+#     for key in sorted(mapping):
+#         prevfreq = freq
+#         freq += mapping[key]
+        
+#         if prevfreq == 0 and freq >0:
+#             startpt = key  
+#         if freq == 0:
+#             endpt = key
+#             if startpt == -1:
+#                 startpt = endpt
+            
+#             ans.append([startpt, endpt])
+#             startpt = endpt = -1
+    
+#     return ans
