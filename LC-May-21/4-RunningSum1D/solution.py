bac16031d04a71a7f229743bf51160ef39066705
Amit Kumar <asrece.86@gmail.com>
2021-05-03 17:29:33 +0530

    problem4

---
+++ LC-May-21/4-RunningSum1D/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        s = 0
+        res = []
+        for num in nums:
+            s += num
+            res.append(s)
+        return res
+            
+            
