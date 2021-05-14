a7f80846bdd8bab09041d5e134232dc42fc67f9d
Amit Kumar <asrece.86@gmail.com>
2021-05-14 14:20:26 +0530

    problem13

---
+++ LC-May-21/13-FlattenBinaryTreeToLinkedList/explantaion.md
@@ -0,0 +1,3 @@
+# Explanation
+
+We solve this using reverse Post-Order tarversal.
---
+++ LC-May-21/13-FlattenBinaryTreeToLinkedList/solution.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flatten(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        if root is None:
+            return 
+        stack = [root]
+        while len(stack) > 0:
+            curr = stack.pop()
+            if curr.right is not None:
+                stack.append(curr.right)
+            if curr.left is not None:
+                stack.append(curr.left)
+            if len(stack) > 0:
+                curr.right = stack[-1]
+            curr.left = None
