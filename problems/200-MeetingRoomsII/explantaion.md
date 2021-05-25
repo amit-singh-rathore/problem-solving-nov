d42a19b83acf7e01205780a15c8418ef43657c3b
Amit Kumar <asrece.86@gmail.com>
2021-05-25 16:37:12 +0530

    problem200

---
+++ problems/200-MeetingRoomsII/explantaion.md
@@ -0,0 +1,3 @@
+# Explanation
+
+For a given intervals we find at any given start or end point how many meeting room is required and then return the max.
---
+++ problems/200-MeetingRoomsII/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        starts = sorted([interval[0] for interval in intervals])
+        ends = sorted([interval[1] for interval in intervals])
+        meet_count, cur_count = 0, 0
+        s_ptr, e_ptr = 0, 0
+        while s_ptr < len(intervals):
+            if starts[s] < ends[e]:
+                s_ptr += 1
+                cur_count += 1
+            else:
+                e_ptr += 1
+                cur_count -= 1
+            meet_count = max(meet_count, cur_count)
+        return meet_count
+        
